7cbe5475b61e5e9de748d20ffa297fbe648faee0
Sovanroth13 <sovanroth016@gmail.com>
2024-03-12 13:21:53 +0700

    update on upload iamge to cloudinary

--- src/cloudinary/cloudinary.provider.ts
+++ src/cloudinary/cloudinary.provider.ts
@@ -6,8 +6,7 @@ export const CloudinaryProvider = {
     return cloudinary.config({
       cloud_name: 'drm2qtfij',
       api_key: '922592464989149',
-      api_secret:
-        'CLOUDINARY_URL=cloudinary://922592464989149:AJGFt1W8sh9YyJTDo6KS4IIkbQU@drm2qtfij',
+      api_secret: 'AJGFt1W8sh9YyJTDo6KS4IIkbQU',
     });
   },
 };
--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -1,20 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { UploadApiErrorResponse, UploadApiResponse, v2 } from 'cloudinary';
-import * as streamifier from 'streamifier';
+import * as streamifier from 'streamifier'; // Import streamifier library
 
 @Injectable()
 export class CloudinaryService {
-  uploadFile(
+  async uploadFile(
     file: Express.Multer.File,
-  ): Promise<UploadApiErrorResponse | UploadApiResponse> {
-    return new Promise<UploadApiErrorResponse | UploadApiResponse>(
-      (resolve, reject) => {
-        const upload = v2.uploader.upload_stream((error, result) => {
-          if (error) reject(error);
-          resolve(result);
-        });
-        streamifier.createReadStream(file.buffer).pipe(upload);
-      },
-    );
+  ): Promise<UploadApiResponse | UploadApiErrorResponse> {
+    return new Promise((resolve, reject) => {
+      const upload = v2.uploader.upload_stream((error, result) => {
+        if (error) return reject(error);
+        resolve(result);
+      });
+
+      // Use streamifier as a function to create a readable stream from the file buffer
+      const readableStream = streamifier.createReadStream(file.buffer);
+
+      readableStream.pipe(upload); // Pipe the file data into the upload stream
+    });
   }
 }
