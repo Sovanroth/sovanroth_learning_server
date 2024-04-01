985b52336ef2ac791580c18ecc82ad9ae4088be4
Sovanroth13 <sovanroth016@gmail.com>
2024-04-01 13:37:32 +0700

    return message

--- src/users/controller/users/users.controller.ts
+++ src/users/controller/users/users.controller.ts
@@ -290,7 +290,7 @@ export class UsersController {
     } catch (error) {
       console.error('Error during forgot password:', error.message);
       return {
-        data: { success: false, message: 'Error during forgot password' },
+        data: { success: false, message: 'Email cannot be found.' },
       };
     }
   }
