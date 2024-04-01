c14f7a0969a43003841ea955bbac1444ccb1e393
Sovanroth13 <sovanroth016@gmail.com>
2024-04-01 18:33:40 +0700

    test token

--- src/users/service/user/user.service.ts
+++ src/users/service/user/user.service.ts
@@ -41,7 +41,7 @@ export class UserService {
       username: user.username,
       email: user.email,
     };
-    const options = { expiresIn: '120s' };
+    const options = { expiresIn: '10s' };
 
     return jwt.sign(payload, 'your-secret-key', options);
   }
